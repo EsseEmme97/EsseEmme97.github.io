8ac157b69b519ce6e699e26b9f42b14fac54f597
sebastian <sebastian9705@gmail.com>
2025-08-18 19:37:46 +0200

    inizio progetto

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web/+esm";
+
+// const canvas = document.querySelector(".canvas");
+
+// const dotLottie = new DotLottie({
+//   canvas,
+//   src: "cac553d0-f289-4332-8148-2e2992859d85.json",
+//   loop: true,
+//   autoplay: true
+// });
